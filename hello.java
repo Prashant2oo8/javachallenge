c1775fe9a689b59054be2dd03f503fb7cd81f70b
Prashant Bhardwaj <prashantbhardwaj0107@gmail.com>
2025-03-22 02:25:03 +0530

    1st

---
+++ hello.java
@@ -0,0 +1,9 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello world! its my day first day for java.");
+        System.out.println(3+7);
+        System.out.println(3.00/2);
+        System.out.println(3*4);
+    }
+
+}
