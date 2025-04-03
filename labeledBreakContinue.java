f198f53ee112eba56937d572975fb50210e11d42
Prashant Bhardwaj <prashantbhardwaj0107@gmail.com>
2025-04-04 04:32:07 +0530

    labeled break and continue statement

---
+++ labeledBreakContinue.java
@@ -0,0 +1,13 @@
+public class labeledBreakContinue {
+    public static void main(String[] args) {
+        outerLoop:
+        for(int i=0;i<5;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+                if(i==2) break outerLoop;
+            }
+            System.out.println();
+        }
+    }
+    
+}
